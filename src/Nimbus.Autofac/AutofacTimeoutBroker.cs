da5a6d555265cda0a48ef05997a3d5bd30042ac0
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-11-05 13:25:07 +1000

    Implemented AutofacTimeoutBroker

---
+++ src/Nimbus.Autofac/AutofacTimeoutBroker.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Nimbus.InfrastructureContracts;
+using Nimbus.MessageContracts;
+
+namespace Nimbus.Autofac
+{
+    public class AutofacTimeoutBroker : ITimeoutBroker
+    {
+        private readonly ILifetimeScope _lifetimeScope;
+
+        public AutofacTimeoutBroker(ILifetimeScope lifetimeScope)
+        {
+            _lifetimeScope = lifetimeScope;
+        }
+
+        public void Dispatch<TBusTimeout>(TBusTimeout busCommand) where TBusTimeout : IBusTimeout
+        {
+            using (var scope = _lifetimeScope.BeginLifetimeScope())
+            {
+                var type = typeof (IHandleTimeout<TBusTimeout>);
+
+                var handler = (IHandleTimeout<IBusTimeout>) scope.Resolve(type);
+                handler.Timeout(busCommand);
+
+            }
+        }
+    }
+}
