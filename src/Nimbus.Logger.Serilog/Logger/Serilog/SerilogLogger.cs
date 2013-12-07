9b8cf8bea098c990a4237a50364e5ef828871738
nblumhardt <nblumhardt@nblumhardt.com>
2013-12-07 10:46:31 +1000

    Added a Serilog implementation of ILogger.

---
+++ src/Nimbus.Logger.Serilog/Logger/Serilog/SerilogLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Serilog;
+using ILogger = Nimbus.InfrastructureContracts.ILogger;
+using ISerilogLogger = Serilog.ILogger;
+
+namespace Nimbus.Logger.Serilog
+{
+    public class SerilogLogger : ILogger
+    {
+        readonly ISerilogLogger _logger;
+
+        public SerilogLogger(ISerilogLogger logger)
+        {
+            if (logger == null) throw new ArgumentNullException("logger");
+            _logger = logger;
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            _logger.Debug(format, args);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            _logger.Information(format, args);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            _logger.Warning(format, args);
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            _logger.Error(format, args);
+        }
+
+        public void Error(Exception exc, string format, params object[] args)
+        {
+            Log.Error(exc, format, args);
+        }
+    }
+}
