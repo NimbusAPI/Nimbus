1adff9bb177bef91b8de2a8dd30d0764d2476b34
michaelnoonan <michaelnoonan@gmail.com>
2013-12-12 21:25:46 +1000

    Simple sample for Windsor.

---
+++ src/PingPong.Windsor/Messages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Nimbus;
+using System.Threading.Tasks;
+using Nimbus.MessageContracts;
+
+namespace PingPong.Windsor
+{
+    public class Ping : BusRequest<Ping, Pong>
+    {
+        public string Message { get; set; }
+    }
+
+    public class Pong : IBusResponse
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ src/PingPong.Windsor/Pinger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Nimbus;
+using Nimbus.InfrastructureContracts;
+using System.Threading.Tasks;
+
+namespace PingPong.Windsor
+{
+    public interface IPinger
+    {
+        Task<string> Ping(string message);
+    }
+
+    public class Pinger : IPinger
+    {
+        private readonly IBus _bus;
+
+        public Pinger(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public async Task<string> Ping(string message)
+        {
+            Console.WriteLine("Sending message: '{0}'", message);
+            var response = await _bus.Request<Ping, Pong>(new Ping { Message = (message.ToLowerInvariant() == "ping" ? "Pong" : message) });
+            return response.Message;
+        }
+    }
+}
