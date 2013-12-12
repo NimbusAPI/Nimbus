0315e044ff9592bccece099f12327661d8d0a3d6
DamianM <damian@damianm.com>
2013-12-12 11:36:34 +1000

    cleaner path sanitisiation. fixes #11

---
+++ src/Nimbus.Tests/InfrastructureTests/PathFactoryTests.cs
@@ -0,0 +1,57 @@
+﻿using Nimbus.Infrastructure;
+using Nimbus.MessageContracts;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Nimbus.Tests.InfrastructureTests
+{
+    [TestFixture]
+    public class PathFactoryTests
+    {
+        [Test]
+        public void WhenCreatingAQueueForANestedType_WeShouldStripOutPlusSigns()
+        {
+
+            var pathName = PathFactory.TopicPathFor(typeof (MyEscapingTestMessages.EscapingTestMessage));
+            pathName.ShouldNotContain("+");
+
+        }
+
+        [Test]
+        public void WhenCreatingAQueue_WeShouldConvertToLowerCase()
+        {
+
+            var pathName = PathFactory.TopicPathFor(typeof (MyEscapingTestMessages.EscapingTestMessage));
+
+            var expectedName = "t." +
+                               typeof (MyEscapingTestMessages.EscapingTestMessage).FullName.Replace("+", ".").ToLower();
+
+            pathName.ShouldBe(expectedName);
+        }
+
+
+        [Test]
+        public void WhenCreatingAQueueForAGenericType_WeShouldStripOutBackticks()
+        {
+            var pathName = PathFactory.QueuePathFor(typeof (MyCommand<string>));
+            pathName.ShouldNotContain("`");
+        }
+
+
+
+        
+    }
+
+    public class MyCommand<T>
+    {
+        public T Metadata { get; set; }
+    }
+
+    public class MyEscapingTestMessages
+    {
+        public class EscapingTestMessage : IBusEvent
+        {
+            
+        }
+    }
+}
--- src/Nimbus/Infrastructure/PathFactory.cs
+++ src/Nimbus/Infrastructure/PathFactory.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Linq;
 
 namespace Nimbus.Infrastructure
 {
     public static class PathFactory
     {
+        // Entity segments can contain only letters, numbers, periods (.), hyphens (-), and underscores.
+        const string QueueCharacterWhitelist = "abcdefghijklmnopqrstuvwxyz1234567890.-";
+
         public static string QueuePathFor(Type type)
         {
             return Sanitize("Q." + type.FullName);
@@ -16,13 +20,22 @@ namespace Nimbus.Infrastructure
 
         private static string Sanitize(string path)
         {
-            // Entity segments can contain only letters, numbers, periods (.), hyphens (-), and underscores.
+            
+            path = string.Join("", path.ToLower().ToCharArray().Select(SanitiseCharacter));
 
-            //FIXME we should tidy this up a bit.  -andrewh 11/12/2013
-            return path
-                .Replace("+", ".")
-                .Replace("`", ".")
-                ;
+
+            return path;
+                
+        }
+
+        private static char SanitiseCharacter(char currentChar)
+        {
+            var whiteList = QueueCharacterWhitelist.ToCharArray();
+
+            if (!whiteList.Contains(currentChar))
+                return '.';
+
+            return currentChar;
         }
     }
 }
