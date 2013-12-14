0732f8cb2bf5890dcc00087e28b54a98d26ffec3
gertjvr <gertjvr@gmail.com>
2013-12-14 22:45:17 +1000

    Added simple IBus.SendAt implementation for future dated commands.

--- src/Nimbus/Infrastructure/Commands/BusCommandSender.cs
+++ src/Nimbus/Infrastructure/Commands/BusCommandSender.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.ServiceBus.Messaging;
 
 namespace Nimbus.Infrastructure.Commands
@@ -6,10 +7,12 @@ namespace Nimbus.Infrastructure.Commands
     internal class BusCommandSender : ICommandSender
     {
         private readonly IMessageSenderFactory _messageSenderFactory;
+        private readonly IClock _clock;
 
-        public BusCommandSender(IMessageSenderFactory messageSenderFactory)
+        public BusCommandSender(IMessageSenderFactory messageSenderFactory, IClock clock)
         {
             _messageSenderFactory = messageSenderFactory;
+            _clock = clock;
         }
 
         public async Task Send<TBusCommand>(TBusCommand busCommand)
@@ -17,6 +20,22 @@ namespace Nimbus.Infrastructure.Commands
             var sender = _messageSenderFactory.GetMessageSender(typeof (TBusCommand));
             var message = new BrokeredMessage(busCommand);
             await sender.SendBatchAsync(new[] {message});
+        }
+
+        public async Task SendAt<TBusCommand>(TimeSpan delay, TBusCommand busCommand)
+        {
+            await SendAt(_clock.UtcNow.Add(delay), busCommand);
+        }
+
+        public async Task SendAt<TBusCommand>(DateTimeOffset proccessAt, TBusCommand busCommand)
+        {
+            var sender = _messageSenderFactory.GetMessageSender(typeof(TBusCommand));
+            var message = new BrokeredMessage(busCommand)
+            {
+                ScheduledEnqueueTimeUtc = proccessAt.DateTime
+            };
+
+            await sender.SendBatchAsync(new[] { message });
         }
     }
 }
