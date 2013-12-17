36412fd43e5ffbc8b04bc41c5aabcc5a99cde0da
Robert Wagner <robert.wagner@readify.net>
2013-12-17 20:31:08 +1000

    Checked the message type is one of those found at bus build time to prevent sending of messages that do not have topics

---
+++ src/Nimbus.IntegrationTests/Tests/SimpleCommandSendingTests/WhenSendingACommandThatIsNotReturedByTheTypeProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nimbus.IntegrationTests.InfrastructureContracts;
+using Nimbus.IntegrationTests.Tests.SimpleCommandSendingTests.MessageContracts;
+using Nimbus.MessageContracts.Exceptions;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Nimbus.IntegrationTests.Tests.SimpleCommandSendingTests
+{
+    public class WhenSendingACommandThatIsNotReturedByTheTypeProvider : TestForAllBuses
+    {
+        public override async Task When(ITestHarnessBusFactory busFactory)
+        {
+            var bus = busFactory.Create();
+
+            var myCommand = new SomeCommandThatIsNotReturedByTheTypeProvider();
+            await bus.Send(myCommand);
+        }
+
+        [Test]
+        [TestCaseSource("AllBusesTestCases")]
+        public async void ABusExceptionIsThrown(ITestHarnessBusFactory busFactory)
+        {
+            try
+            {
+                await When(busFactory);
+                Assert.Fail("Exception expected");
+            }
+            catch (Exception ex)
+            {
+                ex.ShouldBeTypeOf<BusException>();
+                ex.Message.ShouldMatch(@"^The type Nimbus.IntegrationTests.Tests.SimpleCommandSendingTests.MessageContracts.SomeCommandThatIsNotReturedByTheTypeProvider is not a recognised command type\. Ensure it has been registered with the builder with the WithTypesFrom method\.$");
+            }
+        }
+    }
+}
