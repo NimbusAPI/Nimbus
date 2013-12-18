6a13f5a3d36f073fb031c51cd48fc342f52c6569
DamianM <damian@damianm.com>
2013-12-18 14:02:23 +1000

    Registering messages properly

--- src/PizzaSample/Pizza.Ordering/Program.cs
+++ src/PizzaSample/Pizza.Ordering/Program.cs
@@ -19,7 +19,7 @@ namespace Pizza.Ordering
         {
 
             // This is how you tell Nimbus where to find all your message types and handlers.
-            var typeProvider = new AssemblyScanningTypeProvider(Assembly.GetExecutingAssembly(), typeof(NewOrderRecieved).Assembly);
+            var typeProvider = new AssemblyScanningTypeProvider(Assembly.GetExecutingAssembly(), typeof(NewOrderRecieved).Assembly, typeof(OrderPizzaCommand).Assembly);
 
             var messageBroker = new DefaultMessageBroker(typeProvider);
 
