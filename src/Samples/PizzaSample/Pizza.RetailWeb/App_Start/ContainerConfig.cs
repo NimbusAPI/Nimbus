0425658bdaba9af4bc16dcaa78a019b3dd9c0479
uglybugger <andrewh@uglybugger.org>
2014-01-24 15:54:06 +1000

    Added pizza shop retail web app. Only makes one call but demonstrates use in an async controller.

---
+++ src/Samples/PizzaSample/Pizza.RetailWeb/App_Start/ContainerConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace Pizza.RetailWeb.App_Start
+{
+    public static class ContainerConfig
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyModules(typeof (ContainerConfig).Assembly);
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ src/Samples/PizzaSample/Pizza.RetailWeb/AutofacModules/BusModule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+using Autofac;
+using Nimbus;
+using Nimbus.Configuration;
+using Nimbus.Infrastructure;
+using Nimbus.Logger;
+using Pizza.Ordering.Messages;
+using Module = Autofac.Module;
+
+namespace Pizza.RetailWeb.AutofacModules
+{
+    public class BusModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            var connectionString = ConfigurationManager.AppSettings["AzureConnectionString"];
+
+            // You'll want a logger. There's a ConsoleLogger and a NullLogger if you really don't care. You can roll your
+            // own by implementing the ILogger interface if you want to hook it to an existing logging implementation.
+            builder.RegisterType<ConsoleLogger>()
+                   .AsImplementedInterfaces()
+                   .SingleInstance();
+
+            // This is how you tell Nimbus where to find all your message types and handlers.
+            var handlerTypesProvider = new AssemblyScanningTypeProvider(ThisAssembly, typeof(HowLongDoPizzasTakeRequest).Assembly);
+
+            builder.RegisterNimbus(handlerTypesProvider);
+            builder.Register(componentContext => new BusBuilder()
+                                 .Configure()
+                                 .WithConnectionString(connectionString)
+                                 .WithNames("MyApp", Environment.MachineName)
+                                 .WithTypesFrom(handlerTypesProvider)
+                                 .WithAutofacDefaults(componentContext)
+                                 .Build())
+                   .As<IBus>()
+                   .AutoActivate()
+                   .OnActivated(c => c.Instance.Start())
+                   .SingleInstance();
+        }
+    }
+}
