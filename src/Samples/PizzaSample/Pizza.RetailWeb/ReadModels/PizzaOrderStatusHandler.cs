0d6125c289cefa412c61048aceb947a6cb65d957
uglybugger <andrewh@uglybugger.org>
2014-04-18 18:56:47 +1000

    Extracted read model and update handler into their own classes.

---
+++ src/Samples/PizzaSample/Pizza.RetailWeb/ReadModels/PizzaOrderStatusHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nimbus.Handlers;
+using Pizza.Maker.Messages;
+
+namespace Pizza.RetailWeb.ReadModels
+{
+    public class PizzaOrderStatusHandler : IHandleMulticastEvent<NewOrderRecieved>, IHandleMulticastEvent<PizzaIsReady>
+    {
+        private readonly PizzaOrderStatusReadModel _readModel;
+
+        public PizzaOrderStatusHandler(PizzaOrderStatusReadModel readModel)
+        {
+            _readModel = readModel;
+        }
+
+        public async Task Handle(NewOrderRecieved busEvent)
+        {
+            _readModel.AddOrder(busEvent.CustomerName, DateTimeOffset.UtcNow);
+        }
+
+        public async Task Handle(PizzaIsReady busEvent)
+        {
+            _readModel.MarkOrderAsReady(busEvent.CustomerName);
+        }
+    }
+}
--- src/Samples/PizzaSample/Pizza.RetailWeb/ReadModels/PizzaOrderStatusReadModel.cs
+++ src/Samples/PizzaSample/Pizza.RetailWeb/ReadModels/PizzaOrderStatusReadModel.cs
@@ -1,39 +1,35 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Nimbus.Handlers;
-using Pizza.Maker.Messages;
 using Pizza.RetailWeb.Models.Home;
 
 namespace Pizza.RetailWeb.ReadModels
 {
-    public class PizzaOrderStatusReadModel : IAmAReadModel, IHandleMulticastEvent<NewOrderRecieved>, IHandleMulticastEvent<PizzaIsReady>
+    public class PizzaOrderStatusReadModel : IAmAReadModel
     {
         private readonly ConcurrentDictionary<string, PizzaOrderStatus> _orders = new ConcurrentDictionary<string, PizzaOrderStatus>();
-
-        public async Task Handle(NewOrderRecieved busEvent)
-        {
-            var orderStatus = new PizzaOrderStatus
-                              {
-                                  CustomerName = busEvent.CustomerName,
-                                  Ordered = DateTimeOffset.UtcNow,
-                              };
-
-            _orders[busEvent.CustomerName] = orderStatus;
-        }
-
-        public async Task Handle(PizzaIsReady busEvent)
-        {
-            PizzaOrderStatus orderStatus;
-            if (!_orders.TryGetValue(busEvent.CustomerName, out orderStatus)) return;
-
-            orderStatus.Ready = DateTimeOffset.UtcNow;
-        }
 
         public IEnumerable<PizzaOrderStatus> Orders
         {
             get { return _orders.Values; }
+        }
+
+        public void AddOrder(string customerName, DateTimeOffset timestamp)
+        {
+            var orderStatus = new PizzaOrderStatus
+                              {
+                                  CustomerName = customerName,
+                                  Ordered = timestamp,
+                              };
+            _orders[orderStatus.CustomerName] = orderStatus;
+        }
+
+        public void MarkOrderAsReady(string customerName)
+        {
+            PizzaOrderStatus orderStatus;
+            if (!_orders.TryGetValue(customerName, out orderStatus)) return;
+
+            orderStatus.Ready = DateTimeOffset.UtcNow;
         }
     }
 }
