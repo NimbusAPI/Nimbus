1a9295ff878cd3ba2551ace797459a0a33f43407
zshewmaker <zach@azaia.net>
2014-05-21 20:45:02 -0700

    Added Ninject extension to Nimbus.

---
+++ src/Extensions/Nimbus.Ninject/Configuration/NinjectBusBuilderConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Nimbus.DependencyResolution;
+using Ninject;
+
+// ReSharper disable CheckNamespace
+namespace Nimbus.Configuration
+// ReSharper restore CheckNamespace
+{
+	public static class NinjectBusBuilderConfigurationExtensions
+	{
+		public static BusBuilderConfiguration WithNinjectDefaults(this BusBuilderConfiguration configuration, IKernel kernel)
+		{
+			return configuration
+				.WithDependencyResolver(kernel.Get<IDependencyResolver>())
+				.WithLogger(kernel.Get<ILogger>());
+		}
+	}
+}
