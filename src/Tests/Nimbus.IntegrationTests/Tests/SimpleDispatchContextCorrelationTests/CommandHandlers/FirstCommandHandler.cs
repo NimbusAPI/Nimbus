fc061fb5d27cebf632df8b439bb48cbcdbdc0354
Andrew Harcourt <andrewh@uglybugger.org>
2014-06-28 21:58:39 +1000

    Added await.

--- src/Tests/Nimbus.IntegrationTests/Tests/SimpleDispatchContextCorrelationTests/CommandHandlers/FirstCommandHandler.cs
+++ src/Tests/Nimbus.IntegrationTests/Tests/SimpleDispatchContextCorrelationTests/CommandHandlers/FirstCommandHandler.cs
@@ -2,17 +2,16 @@
 using Nimbus.Handlers;
 using Nimbus.IntegrationTests.Tests.SimpleDispatchContextCorrelationTests.MessageContracts;
 
-#pragma warning disable 4014
-
 namespace Nimbus.IntegrationTests.Tests.SimpleDispatchContextCorrelationTests.CommandHandlers
 {
     public class FirstCommandHandler : IHandleCommand<FirstCommand>
     {
+        // provided by our test harness setup so that we don't need a container.
         internal static IBus Bus;
 
         public async Task Handle(FirstCommand busCommand)
         {
-            Bus.Send(new SecondCommand());
+            await Bus.Send(new SecondCommand());
         }
     }
 }
