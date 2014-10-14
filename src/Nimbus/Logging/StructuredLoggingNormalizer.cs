1053dc52b378685e7c3f7d98ef20f24288f97188
Andrew Harcourt <andrewh@uglybugger.org>
2014-10-14 23:38:22 +1000

    Tidied up extension method.

--- src/Nimbus/Logging/StructuredLoggingNormalizer.cs
+++ src/Nimbus/Logging/StructuredLoggingNormalizer.cs
@@ -8,15 +8,25 @@ namespace Nimbus.Logging
     {
         private static readonly Regex _regex = new Regex(@"{\S+?}", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
 
-        public static string NormalizeToStringFormat(this string format)
+        internal static string NormalizeToStringFormat(this string format)
+        {
+            return Normalize(format);
+        }
+
+        /// <summary>
+        ///     Normalizes a log format string containing named placeholders (e.g. Message: {MessageId}) to a string containing
+        ///     only
+        ///     numeric placeholders (e.g. Message {0}).
+        /// </summary>
+        public static string Normalize(string format)
         {
             var output = format;
 
             var matchCollection = _regex.Matches(format);
             var matches = matchCollection
-                                .Cast<Match>()
-                                .DistinctBy(m => m.Captures[0].Value)
-                                .ToArray();
+                .Cast<Match>()
+                .DistinctBy(m => m.Captures[0].Value)
+                .ToArray();
 
             for (var i = 0; i < matches.Length; i++)
             {
