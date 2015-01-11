db52e185827e589e1f4e3cb9975c34a37e26c5ce
Ben Gale <ben.gale@4com.co.uk>
2015-01-11 19:57:51 +0000

    Added an extension to use protobuf for serialization.

---
+++ src/Extensions/Nimbus.Serializers.ProtoBuf/ProtoBufSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+
+namespace Nimbus.Serializers.ProtoBuf
+{
+    public class ProtoBufSerializer : ISerializer
+    {
+        public string Serialize(object serializableObject)
+        {
+            var stream = new MemoryStream();
+            Serializer.Serialize(stream, serializableObject);
+            return Convert.ToBase64String(
+                stream.GetBuffer(), 0, (int)stream.Length);
+        }
+
+        public object Deserialize(string serializedObject, Type type)
+        {
+            var bytes = Convert.FromBase64String(serializedObject);
+            var stream = new MemoryStream(bytes);
+
+            var method = typeof(Serializer).GetMethod("Deserialize");
+            method = method.MakeGenericMethod(type);
+
+            return method.Invoke(this, new object[]{stream});
+        }
+    }
+}
