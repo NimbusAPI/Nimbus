d243517d441000f239314abc3cf92c9fc2c177dc
Stuart Blackler <stuart.blackler@4com.co.uk>
2015-01-12 16:44:01 +0000

    Initial Commit of Nimbus-Unity

---
+++ src/Extensions/Nimbus.Unity/Infastructure/UnityDependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Unity;
+using Nimbus.DependencyResolution;
+
+namespace Nimbus.Unity.Infastructure
+{
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+        private readonly IUnityContainer _unityContainer;
+
+        public UnityDependencyResolver(IUnityContainer unityContainer)
+        {
+            _unityContainer = unityContainer;
+        }
+
+        public IDependencyResolverScope CreateChildScope()
+        {
+            return new UnityDependencyResolverScope(_unityContainer.CreateChildContainer());
+        }
+
+        public void Dispose()
+        {
+            _unityContainer.Dispose();
+        }
+    }
+}
---
+++ src/Samples/PingPong.Unity/Ping.cs
@@ -0,0 +1,9 @@
+﻿using Nimbus.MessageContracts;
+
+namespace PingPong.Unity
+{
+    public class Ping : BusRequest<Ping, Pong>
+    {
+        public string Message { get; set; }
+    }
+}
