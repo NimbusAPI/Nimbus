0dced967b23489c53c8d63b1661ed7ccb499b5e6
Jacques Bosch <jfbosch@gmail.com>
2015-05-18 22:28:15 +0200

    update nuspec file

--- src/Nimbus/Properties/AssemblyInfo.cs
+++ src/Nimbus/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Nimbus")]
+[assembly: AssemblyTitle("Anubus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nimbus")]
+[assembly: AssemblyProduct("Anubus")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
