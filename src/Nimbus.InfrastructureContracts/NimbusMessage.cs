7c2ef6839061dc1514f4fb7b25df0fb72e411e2c
Andrew Harcourt <andrewh@uglybugger.org>
2015-12-13 18:21:17 +1000

    Initialised some properties with some temporary defaults.

--- src/Nimbus.InfrastructureContracts/NimbusMessage.cs
+++ src/Nimbus.InfrastructureContracts/NimbusMessage.cs
@@ -6,35 +6,39 @@ namespace Nimbus.Infrastructure
 {
     public class NimbusMessage
     {
-        public NimbusMessage(byte[] payload)
+        public NimbusMessage(byte[] payload) : this()
         {
             Payload = payload;
         }
 
         public NimbusMessage()
         {
+            TimeToLive = TimeSpan.FromMinutes(30);
+            Properties = new Dictionary<string, object>();
         }
 
         public Guid CorrelationId { get; set; }
         public string ReplyTo { get; set; }
         public TimeSpan TimeToLive { get; set; }
         public DateTime ScheduledEnqueueTimeUtc { get; set; }
-        public Dictionary<string, object> Properties { get; set; }
+        public IDictionary<string, object> Properties { get; set; }
         public Guid MessageId { get; set; }
         public byte[] Payload { get; set; }
-
 
         public DateTimeOffset LockedUntilUtc { get; set; }
         public int Size { get; set; }
 
+        [Obsolete("We'll be deleting this shortly.")]
         public async Task RenewLockAsync()
         {
         }
 
+        [Obsolete("We'll be deleting this shortly.")]
         public async Task CompleteAsync()
         {
         }
 
+        [Obsolete("We'll be deleting this shortly.")]
         public async Task AbandonAsync(Dictionary<string, object> exceptionDetailsAsProperties)
         {
         }
