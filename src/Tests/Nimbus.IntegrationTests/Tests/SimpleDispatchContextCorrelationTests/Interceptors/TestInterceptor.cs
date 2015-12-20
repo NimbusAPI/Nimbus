8fe36f620e2b510c409ca3f1d00c6e4e9c354842
Andrew Harcourt <andrewh@uglybugger.org>
2015-12-20 22:39:40 +1000

    Fixed .Clear() bug in test interceptor (it wasn't clearing the list of intercepted messages between tests).

--- src/Tests/Nimbus.IntegrationTests/Tests/SimpleDispatchContextCorrelationTests/Interceptors/TestInterceptor.cs
+++ src/Tests/Nimbus.IntegrationTests/Tests/SimpleDispatchContextCorrelationTests/Interceptors/TestInterceptor.cs
@@ -5,23 +5,24 @@ using Nimbus.PropertyInjection;
 
 namespace Nimbus.IntegrationTests.Tests.SimpleDispatchContextCorrelationTests.Interceptors
 {
-    public class TestInterceptor : InboundInterceptor, IRequireDispatchContext
+    public class TestInterceptor : InboundInterceptor, IRequireDispatchContext, IRequireNimbusMessage
     {
         internal static readonly List<IDispatchContext> DispatchContexts = new List<IDispatchContext>();
         internal static readonly List<NimbusMessage> NimbusMessages = new List<NimbusMessage>();
 
         public IDispatchContext DispatchContext { get; set; }
-        public object BrokeredMessage { get; set; }
+        public NimbusMessage NimbusMessage { get; set; }
 
         public override async Task OnCommandHandlerExecuting<TBusCommand>(TBusCommand busCommand, NimbusMessage nimbusMessage)
         {
             DispatchContexts.Add(DispatchContext);
-            NimbusMessages.Add(nimbusMessage);
+            NimbusMessages.Add(NimbusMessage);
         }
 
         public static void Clear()
         {
             DispatchContexts.Clear();
+            NimbusMessages.Clear();
         }
     }
 }
