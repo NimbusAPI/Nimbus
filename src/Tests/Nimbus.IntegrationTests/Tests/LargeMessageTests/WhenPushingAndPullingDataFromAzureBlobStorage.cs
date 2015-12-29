ddc3c68b88218cfe61adba73ad20bd644c671bf3
Andrew Harcourt <andrewh@uglybugger.org>
2015-12-30 09:26:01 +1000

    Removed asserting stopwatch. Our test timeouts will take care of timing now.

--- src/Tests/Nimbus.IntegrationTests/Tests/LargeMessageTests/WhenPushingAndPullingDataFromAzureBlobStorage.cs
+++ src/Tests/Nimbus.IntegrationTests/Tests/LargeMessageTests/WhenPushingAndPullingDataFromAzureBlobStorage.cs
@@ -5,10 +5,8 @@ using System.Threading.Tasks;
 using ConfigInjector.QuickAndDirty;
 using Nimbus.LargeMessages.Azure.Client;
 using Nimbus.LargeMessages.Azure.Configuration.Settings;
-using Nimbus.Tests.Common;
 using Nimbus.Tests.Common.Configuration;
 using Nimbus.Tests.Common.Stubs;
-using Nimbus.Tests.Common.TestUtilities;
 using NUnit.Framework;
 using Shouldly;
 
@@ -37,29 +35,18 @@ namespace Nimbus.IntegrationTests.Tests.LargeMessageTests
             _bytes = Encoding.UTF8.GetBytes(Enumerable.Range(0, 1024).Select(i => '.').ToArray());
             _expiresAfter = DateTimeOffset.UtcNow.AddDays(1);
 
-            using (new AssertingStopwatch("Store", TimeSpan.FromSeconds(TimeoutSeconds)))
-            {
-                _storageKey = await Subject.Store(_id, _bytes, _expiresAfter);
-            }
-            Console.WriteLine();
-            Console.WriteLine();
+            _storageKey = await Subject.Store(_id, _bytes, _expiresAfter);
         }
 
         [Test]
         public async Task TheRetrievedValueShouldBeTheSameAsTheStoredValue()
         {
-            using (new AssertingStopwatch("Retrieve", TimeSpan.FromSeconds(TimeoutSeconds)))
-            {
-                var retrieved = await Subject.Retrieve(_storageKey);
-                retrieved.ShouldBe(_bytes);
-            }
+            var retrieved = await Subject.Retrieve(_storageKey);
+            retrieved.ShouldBe(_bytes);
         }
 
         public override void TearDown()
         {
-            Console.WriteLine();
-            Console.WriteLine();
-
             Subject.Delete(_storageKey).Wait();
 
             base.TearDown();
