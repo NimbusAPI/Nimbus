8d8f8359584e755dcaab2fdcce754e4d6e589002
Andrew Harcourt <andrewh@uglybugger.org>
2015-12-30 12:18:25 +1000

    Fixed conventions

--- src/Nimbus/Extensions/FluentConfigurationExtensions.cs
+++ src/Nimbus/Extensions/FluentConfigurationExtensions.cs
@@ -4,12 +4,12 @@ namespace Nimbus.Extensions
 {
     internal static class FluentConfigurationExtensions
     {
-        public static T Chain<T>(this T t, Func<T, T> apply)
+        internal static T Chain<T>(this T t, Func<T, T> apply)
         {
             return apply(t);
         }
 
-        public static T Chain<T>(this T t, Action<T> apply)
+        internal static T Chain<T>(this T t, Action<T> apply)
         {
             apply(t);
             return t;
