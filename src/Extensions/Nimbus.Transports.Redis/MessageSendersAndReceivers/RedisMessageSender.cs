97ebeb309580982f6357ecee05608c0c1c4d3671
Andrew Harcourt <andrewh@uglybugger.org>
2016-01-09 12:47:34 +1000

    Yet more debug logging

--- src/Extensions/Nimbus.Transports.Redis/MessageSendersAndReceivers/RedisMessageSender.cs
+++ src/Extensions/Nimbus.Transports.Redis/MessageSendersAndReceivers/RedisMessageSender.cs
@@ -10,20 +10,26 @@ namespace Nimbus.Transports.Redis.MessageSendersAndReceivers
         private readonly string _redisKey;
         private readonly ISerializer _serializer;
         private readonly Func<IDatabase> _databaseFunc;
+        private readonly ILogger _logger;
 
-        public RedisMessageSender(string redisKey, ISerializer serializer, Func<IDatabase> databaseFunc)
+        public RedisMessageSender(string redisKey, ISerializer serializer, Func<IDatabase> databaseFunc, ILogger logger)
         {
             _redisKey = redisKey;
             _serializer = serializer;
             _databaseFunc = databaseFunc;
+            _logger = logger;
         }
 
         public async Task Send(NimbusMessage message)
         {
             var serialized = _serializer.Serialize(message);
             var database = _databaseFunc();
-            await database.ListRightPushAsync(_redisKey, serialized);
-            await database.PublishAsync(_redisKey, string.Empty);
+
+            var pushResult = await database.ListRightPushAsync(_redisKey, serialized);
+            _logger.Debug($"Redis {nameof(database.ListRightPushAsync)} returned {{RedisReturnCode}}", pushResult);
+
+            var publishResult = await database.PublishAsync(_redisKey, string.Empty);
+            _logger.Debug($"Redis {nameof(database.PublishAsync)} returned {{RedisReturnCode}}", publishResult);
         }
     }
 }
