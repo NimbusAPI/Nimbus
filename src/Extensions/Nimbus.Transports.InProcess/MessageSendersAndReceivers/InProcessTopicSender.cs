06c1c1585d884fa7cc5499e3fda83df9f906a3e8
Andrew Harcourt <andrewh@uglybugger.org>
2016-01-09 15:11:24 +1000

    Added logger warnings when we send to a queue/topic which does not have a listener.

--- src/Extensions/Nimbus.Transports.InProcess/MessageSendersAndReceivers/InProcessTopicSender.cs
+++ src/Extensions/Nimbus.Transports.InProcess/MessageSendersAndReceivers/InProcessTopicSender.cs
@@ -12,12 +12,14 @@ namespace Nimbus.Transports.InProcess.MessageSendersAndReceivers
         private readonly ISerializer _serializer;
         private readonly Topic _topic;
         private readonly InProcessMessageStore _messageStore;
+        private readonly ILogger _logger;
 
-        public InProcessTopicSender(ISerializer serializer, Topic topic, InProcessMessageStore messageStore)
+        public InProcessTopicSender(ISerializer serializer, Topic topic, InProcessMessageStore messageStore, ILogger logger)
         {
             _serializer = serializer;
             _topic = topic;
             _messageStore = messageStore;
+            _logger = logger;
         }
 
         public async Task Send(NimbusMessage message)
@@ -30,7 +32,11 @@ namespace Nimbus.Transports.InProcess.MessageSendersAndReceivers
         {
             var clone = Clone(message);
             AsyncBlockingCollection<NimbusMessage> topicQueue;
-            if (!_messageStore.TryGetExistingMessageQueue(_topic.TopicPath, out topicQueue)) return;
+            if (!_messageStore.TryGetExistingMessageQueue(_topic.TopicPath, out topicQueue))
+            {
+                _logger.Warn("A message was sent to an in-process topic {TopicPath} but nobody is listening on that topic. Not sending.", _topic.TopicPath);
+                return;
+            }
             await topicQueue.Add(clone);
         }
 
