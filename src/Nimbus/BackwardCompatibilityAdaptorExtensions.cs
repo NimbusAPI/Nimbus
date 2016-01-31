8053cedc51401c838bc31294a3addb806b8ca4ad
Andrew Harcourt <andrewh@uglybugger.org>
2016-01-31 19:09:12 +1000

    Set obsolete Bus.Defer extension methods to error rather than warn. We'll remove these shortly.

--- src/Nimbus/BackwardCompatibilityAdaptorExtensions.cs
+++ src/Nimbus/BackwardCompatibilityAdaptorExtensions.cs
@@ -6,14 +6,14 @@ namespace Nimbus
 {
     public static class BackwardCompatibilityAdaptorExtensions
     {
-        [Obsolete("Deprecated in favour of .SendAt(...) and .SendAfter(...) methods. This adaptor method will be removed in a future release.")]
+        [Obsolete("Deprecated in favour of .SendAt(...) and .SendAfter(...) methods. This adaptor method will be removed in a future release.", true)]
         public static Task Defer<TBusCommand>(this IBus bus, TimeSpan delay, TBusCommand busCommand) where TBusCommand : IBusCommand
         {
             var deliveryTime = DateTimeOffset.UtcNow.Add(delay);
             return bus.SendAt(busCommand, deliveryTime);
         }
 
-        [Obsolete("Deprecated in favour of .SendAt(...) and .SendAfter(...) methods. This adaptor method will be removed in a future release.")]
+        [Obsolete("Deprecated in favour of .SendAt(...) and .SendAfter(...) methods. This adaptor method will be removed in a future release.", true)]
         public static Task Defer<TBusCommand>(this IBus bus, DateTimeOffset processAt, TBusCommand busCommand) where TBusCommand : IBusCommand
         {
             return bus.SendAt(busCommand, processAt);
