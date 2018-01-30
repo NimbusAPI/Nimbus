13353f1698c6aeb3527efc5274338a5e44b27315
Jeremy Cade <me@jeremycade.com>
2018-01-30 10:37:34 +1000

    Migrate Serilog Extension

---
+++ src/extensions/Nimbus.Logger.Serilog/Logger/Serilog/SerilogLogger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ISerilogLogger = Serilog.ILogger;
+
+namespace Nimbus.Logger.Serilog
+{
+    public class SerilogLogger : ILogger
+    {
+        private readonly ISerilogLogger _logger;
+
+        public SerilogLogger(ISerilogLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            _logger.Debug(format, args);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            _logger.Information(format, args);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            _logger.Warning(format, args);
+        }
+
+        public void Warn(Exception exc, string format, params object[] args)
+        {
+            _logger.Warning(exc, format, args);
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            _logger.Error(format, args);
+        }
+
+        public void Error(Exception exc, string format, params object[] args)
+        {
+            _logger.Error(exc, format, args);
+        }
+    }
+}
---
+++ src/extensions/Nimbus.Logger.Serilog/Logger/Serilog/SerilogStaticLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Serilog;
+
+namespace Nimbus.Logger.Serilog
+{
+    public class SerilogStaticLogger : ILogger
+    {
+        public void Debug(string format, params object[] args)
+        {
+            Log.Debug(format, args);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            Log.Information(format, args);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            Log.Warning(format, args);
+        }
+
+        public void Warn(Exception exc, string format, params object[] args)
+        {
+            Log.Warning(exc, format, args);
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            Log.Error(format, args);
+        }
+
+        public void Error(Exception exc, string format, params object[] args)
+        {
+            Log.Error(exc, format, args);
+        }
+    }
+}
