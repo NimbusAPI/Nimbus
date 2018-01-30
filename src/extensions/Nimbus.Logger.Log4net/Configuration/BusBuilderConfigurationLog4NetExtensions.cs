f1ef79f20035b02d1612e62199810f571f156798
Jeremy Cade <me@jeremycade.com>
2018-01-30 10:37:09 +1000

    Migrate Log4Net Extension

---
+++ src/extensions/Nimbus.Logger.Log4net/Configuration/BusBuilderConfigurationLog4NetExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Nimbus.Logger.Log4net;
+using log4net;
+
+namespace Nimbus.Configuration
+{
+    /// <summary>
+    ///     Extends <see cref="BusBuilderConfiguration" /> with support for the Log4net logging framework.
+    /// </summary>
+    public static class BusBuilderConfigurationLog4NetExtensions
+    {
+        /// <summary>
+        ///     Log to the provided Log4net <see cref="log4net.ILog" />.
+        /// </summary>
+        /// <param name="configuration">The bus configuration to apply the logger to.</param>
+        /// <param name="logger">The logger.</param>
+        /// <returns>Bus configuration.</returns>
+        public static BusBuilderConfiguration WithLog4netLogger(this BusBuilderConfiguration configuration, ILog logger)
+        {
+            return configuration
+                .WithLogger(new Log4NetLogger(logger));
+        }
+    }
+}
---
+++ src/extensions/Nimbus.Logger.Log4net/Logger/Log4Net/Log4netLogger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using log4net;
+using Nimbus.Infrastructure.Logging;
+
+namespace Nimbus.Logger.Log4net
+{
+    public class Log4NetLogger : ILogger
+    {
+        private readonly ILog _log;
+
+        public Log4NetLogger(ILog log)
+        {
+            _log = log;
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            if (args.Length > 0)
+                _log.DebugFormat(StructuredLoggingNormalizer.Normalize(format), args);
+            else
+                _log.Debug(format);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            if (args.Length > 0)
+                _log.InfoFormat(StructuredLoggingNormalizer.Normalize(format), args);
+            else
+                _log.Info(format);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            if (args.Length > 0)
+                _log.WarnFormat(StructuredLoggingNormalizer.Normalize(format), args);
+            else
+                _log.Warn(format);
+        }
+
+        public void Warn(Exception exc, string format, params object[] args)
+        {
+            _log.Warn(string.Format(format, args), exc);
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            if (args.Length > 0)
+                _log.ErrorFormat(StructuredLoggingNormalizer.Normalize(format), args);
+            else
+                _log.Error(format);
+        }
+
+        public void Error(Exception exc, string format, params object[] args)
+        {
+            var message = (args.Length) > 0 ? String.Format(StructuredLoggingNormalizer.Normalize(format), args) : format;
+            _log.Error(message, exc);
+        }
+    }
+}
