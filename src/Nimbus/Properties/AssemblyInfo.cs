3afdf6b3d76c9ee52123787594888c9d10816af6
damianmac <damian@damianm.com>
2018-09-23 17:17:10 +1000

    Added in autofac support

---
+++ src/Nimbus/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Nimbus.Tests.Common", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Nimbus.UnitTests", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Nimbus.IntegrationTests", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Nimbus.StressTests", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Nimbus.Autofac")]
+[assembly: InternalsVisibleTo("Nimbus.Windsor")]
+[assembly: InternalsVisibleTo("Nimbus.LargeMessages.Azure")]
+[assembly: InternalsVisibleTo("Nimbus.LargeMessages.FileSystem")]
+[assembly: InternalsVisibleTo("Nimbus.Logger.Serilog")]
+[assembly: InternalsVisibleTo("Nimbus.Transports.InProcess")]
+[assembly: InternalsVisibleTo("Nimbus.Transports.Redis")]
+[assembly: InternalsVisibleTo("Nimbus.Transports.AzureServiceBus")]
+[assembly: InternalsVisibleTo("Nimbus.Transports.WindowsServiceBus")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2", AllInternalsVisible = true)]
