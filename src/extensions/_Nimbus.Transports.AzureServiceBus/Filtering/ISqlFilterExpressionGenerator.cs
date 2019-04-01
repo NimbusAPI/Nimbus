c3b349933d911ae12bb21fc735bb6c5134990593
Jeremy Cade <me@jeremycade.com>
2019-04-01 14:24:29 +1000

    Remove old .NET FX libraries

--- src/extensions/_Nimbus.Transports.AzureServiceBus/Filtering/ISqlFilterExpressionGenerator.cs
+++
@@ -1,9 +0,0 @@
-using Nimbus.Filtering.Conditions;
-
-namespace Nimbus.Transports.AzureServiceBus.Filtering
-{
-    internal interface ISqlFilterExpressionGenerator
-    {
-        string GenerateFor(IFilterCondition filterCondition);
-    }
-}
