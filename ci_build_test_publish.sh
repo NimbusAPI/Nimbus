7b78aeedac4c62ce17f506dbfda7780ee27c6a8e
damianmac <damian@damianm.com>
2021-06-14 14:42:06 +1000

    added gitversions

--- ci_build_test_publish.sh
+++ ci_build_test_publish.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
+export BUILD_NUMBER=$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.3.5-linux-alpine.3.10-x64-netcoreapp3.1 /repo /showvariable semver | tr -dc '[[:print:]]')
+echo $BUILD_NUMBER
 DOCKER_COMPOSE="docker-compose -f docker-compose.yml -f docker-compose.build.yml"
 $DOCKER_COMPOSE build --build-arg $BUILD_NUMBER
 $DOCKER_COMPOSE up -d
+
 $DOCKER_COMPOSE run --rm nimbus dotnet cake --Target="Test"
 $DOCKER_COMPOSE run --rm -e REDIS_TEST_CONNECTION=redis nimbus dotnet cake --Target="IntegrationTest"
 $DOCKER_COMPOSE run --rm -e NUGET_API_KEY=$NUGET_API_KEY nimbus dotnet cake --Target="PushPackages"
